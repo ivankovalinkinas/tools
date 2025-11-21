2b5c798cb1c15ef80ad1467c917eae8ab830669c
Ivan Kovalinkinas <novekovas@gmail.com>
2025-11-21 09:25:46 -0300

    Adicionado projeto My_Local_IP em C#

---
+++ C#/My_Local_IP_Console/Main.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+
+StringBuilder sb = new StringBuilder();
+sb.AppendLine("Hostname: " + Environment.MachineName);
+sb.AppendLine("");
+
+foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
+{
+    foreach (UnicastIPAddressInformation ip in ni.GetIPProperties().UnicastAddresses)
+    {
+        if (ip.Address.AddressFamily == AddressFamily.InterNetwork && 
+            !ip.Address.ToString().StartsWith("169.") && 
+            ip.Address.ToString() != "127.0.0.1")
+        {
+            sb.AppendLine("Interface: " + ni.Name);
+            sb.AppendLine("IPv4: " + ip.Address.ToString());
+            sb.AppendLine("");
+        }
+    }
+}
+
+Console.WriteLine(sb.ToString().Trim());
+
+Console.WriteLine("\n\nInformações exibidas! \nPressione qualquer tecla para sair.");
+Console.ReadKey();
